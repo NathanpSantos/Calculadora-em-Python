e7b1f57132d305022a6785db1e95941f1f1dc299
NathanpSantos <124455305+NathanpSantos@users.noreply.github.com>
2023-04-16 18:39:43 -0300

    Calculadora em Python

---
+++ App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="output">
+            <div className="output">
+                <button>C</button>
+                <button></button>
+                <button>=</button>
+                <button>
+                    <svg xmlsn="http://wwww.w3.org/2000/svg" class="icon icon-tabler icon-tabler-backspace" width="36  </button>    
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M20 6a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-111-5 -5a1.5 1.5 0 0 1 0 -215 -5z"></path>
+                    <path> d="M12 1014 4N0 -41-4 4"><</path>"
+                  </svg>
+                    </button>1</button>
+                    </button>2</button>
+                    </button>3</button>
+                    </button>4</button>
+                    </button>5</button>
+                    </button>6</button>
+                    </button>7</button>
+                    </button>8</button>
+                    </button>9</button>
+                    </button>
+                    </button>
+                    </button>
+                    </button>
+                       
+        </div>
+    )
+}
